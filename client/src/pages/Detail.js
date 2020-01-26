e88d30b5236952e9cce905830fdf4d679726f4c6
Kyle LaMont <kylerlamont@gmail.com>
2020-01-26 14:06:52 -0800

    updated react page with new info from db

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -35,7 +35,7 @@ class Detail extends Component {
           <Col size="md-10 md-offset-1">
             <article>
               <h1>Synopsis</h1>
-              <p>{this.state.book.synopsis}</p>
+              <p>{this.state.book.description}</p>
             </article>
           </Col>
         </Row>
